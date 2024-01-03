0dd73b435b1b0bdd6bbb87d2b70c3431285137aa
SinhaBindu <sinhabinduk@gmail.com>
2024-01-03 21:16:26 +0530

    commited

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -46,18 +46,18 @@ namespace FP
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
-                AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
+               // AllowOnlyAlphanumericUserNames = false,
+               // RequireUniqueEmail = true
             };
 
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequiredLength = 10,
+                //RequireNonLetterOrDigit = true,
                 RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                //RequireLowercase = true,
+                //RequireUppercase = true,
             };
 
             // Configure user lockout defaults
--- Scripts/JS/Master.js
+++ Scripts/JS/Master.js
@@ -62,6 +62,14 @@ function validate(evt) {
         if (theEvent.preventDefault) theEvent.preventDefault();
     }
 }
+ 
+$(".numberonly").on("input", function (evt) {
+    var self = $(this);
+    self.val(self.val().replace(/[^0-9.]/g, ''));
+    if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57)) {
+        evt.preventDefault();
+    }
+});
 
 /* -------------------------------------------- Date Range -------------------------------*/
 
