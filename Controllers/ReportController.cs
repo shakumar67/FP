13e1610506068b66378778c690e536b8248220d2
HS553 <sinhaharshit829@gmail.com>
2024-01-26 23:25:33 +0530

    Working on letter two

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,59 @@
+﻿using FP.Manager;
+using FP.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FP.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LetterTwo()
+        {
+            FilterModel model = new FilterModel();
+
+            return View(model);
+            
+        }
+        public ActionResult GetLetterTwo(FilterModel model)
+        {
+            try
+            {
+                bool IsCheck = false;
+                var tbllist = SP_Model.SpLetterTwo(model);
+                if (tbllist.Rows.Count > 0)
+                {
+                    IsCheck = true;
+                }
+                var html = ConvertViewToString("_LetterTwoData", tbllist);
+                var res = Json(new { IsSuccess = IsCheck, Data = html }, JsonRequestBehavior.AllowGet);
+                res.MaxJsonLength = int.MaxValue;
+                return res;
+            }
+            catch (Exception ex)
+            {
+                string er = ex.Message;
+                return Json(new { IsSuccess = false, Data = "" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        private string ConvertViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (StringWriter writer = new StringWriter())
+            {
+                ViewEngineResult vResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                ViewContext vContext = new ViewContext(this.ControllerContext, vResult.View, ViewData, new TempDataDictionary(), writer);
+                vResult.View.Render(vContext, writer);
+                return writer.ToString();
+            }
+        }
+    }
+}
--- Manager/SP_Model.cs
+++ Manager/SP_Model.cs
@@ -128,5 +128,23 @@ namespace FP.Manager
         }
         #endregion
 
+        #region Report Letter
+        public static DataTable SpLetterTwo(FilterModel model)
+        {
+            StoredProcedure sp = new StoredProcedure("Usp_LetterTwo");
+            sp.Command.AddParameter("@DisId", model.DistrictId, DbType.String);
+            sp.Command.AddParameter("@BlkId", model.BlockId, DbType.String);
+            sp.Command.AddParameter("@PytId", model.PanchayatId, DbType.String);
+            sp.Command.AddParameter("@VoId", model.VOId, DbType.String);
+            sp.Command.AddParameter("@Month", model.Month, DbType.String);
+            sp.Command.AddParameter("@Year", model.Year, DbType.String);
+            sp.Command.AddParameter("@Role", model.RoleId, DbType.String);
+            sp.Command.AddParameter("@CutUser", model.CutUser, DbType.String);
+            DataTable dt = sp.ExecuteDataSet().Tables[0];
+            return dt;
+        }
+
+        #endregion
+
     }
 }
