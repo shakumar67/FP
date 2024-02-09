6cf9874db27646638e6f050792f42413da47f2e0
SinhaBindu <sinhabinduk@gmail.com>
2024-02-09 11:38:56 +0530

    Eligible Couples Parity Child

--- Scripts/JS/ChartChild.js
+++ Scripts/JS/ChartChild.js
@@ -27,21 +27,18 @@ function GetDataChild() {
                // debugger
                 var resdata = JSON.parse(resp.Data);
                 var resD = resdata;
-                var DatalistMale = [], DatalistFemale = [], DList=[],DListM = [],DListF=[];
+                var Datalist = [], Datasum = [], DList=[],DListM = [],DListF=[];
                 var Dobj = {};
                 for (var i = 0; i < resD.length; i++) {
-                    DatalistMale.push({ name: "0 Child", value: resD[i].MaleC0 });//shortName
-                    DatalistMale.push({ name: "1 Child", value: resD[i].MaleC1 });
-                    DatalistMale.push({ name: "2 Child", value: resD[i].MaleC2 });
-                    DatalistMale.push({ name: "2+ Child", value: resD[i].MaleCmore });
-
-                    DatalistFemale.push({ name: "0 Child", value: resD[i].FemaleC0 });
-                    DatalistFemale.push({ name: "1 Child", value: resD[i].FemaleC1 });
-                    DatalistFemale.push({ name: "2 Child", value: resD[i].FemaleC2 });
-                    DatalistFemale.push({ name: "2+ Child", value: resD[i].FemaleCmore });
+                   Datalist.push({ name: "P-0", value: resD[i].P0 });//shortName
+                   Datalist.push({ name: "P-1", value: resD[i].P1 });
+                   Datalist.push({ name: "P-2", value: resD[i].P2 });
+                   Datalist.push({ name: "P-2+", value: resD[i].PPlus });
+                   Datasum += resD[i].TotalPlus;
+                  
                 }
-                DList.push({ name: "Male Child", data: DatalistMale });
-                DList.push({ name: "Female Child", data: DatalistFemale });
+                DList.push({ name: "Eligible Couples", data: Datalist });
+               // DList.push({ name: "Female Child", data: DatalistFemale });
                 //Dobj = [DListM, DListF];
 
                 Highcharts.chart('childddmf', {
@@ -50,16 +47,16 @@ function GetDataChild() {
                         height: '100%'
                     },
                     title: {
-                        text: 'No of male & female child at present',
+                        text: 'Eligible Couples (Parity - 0/1/2/2+)',
                         align: 'center'
                     },
                     subtitle: {
-                       //  text: 'Line-Listing',
-                        align: 'left'
+                        text: '(0 Child, 1 Child, 2 Child, 2+ Child) ' + '<b> Total : '+ Datasum+'</b>',
+                        align: 'center'
                     },
                     tooltip: {
                         useHTML: true,
-                        pointFormat: '<b>{point.name}:</b> {point.value}<sub></sub>'
+                        pointFormat: '<b>{point.name}: </b> {point.value}<sub></sub>'
                     },
                     credits: {
                         enabled: false
@@ -119,8 +116,16 @@ function GetDataChild() {
                             dataLabels: {
                                 enabled: true,
                                 // format: '{point.shortName}',
-                                parentNodeFormat: '{point.series.name}'
-                            }
+                                parentNodeFormat: '{point.series.name}',
+                                style: {
+                                    color: 'black',
+                                    textOutline: 'none',
+                                    fontWeight: 'normal',
+                                    fontSize: '18px'
+                                }
+
+                            },
+
                         }
                     },
                     series: DList,
